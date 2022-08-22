7165d733202efb0e85e876ba2f620f12e9cd2ee1
canbulgay <canbulgay@outlook.com>
2022-08-22 12:23:23 +0300

    feat(jta): Create TelegramClientController , add getUserTelegramCredentials method in it.

---
+++ v1/src/controllers/TelegramClientController.js
@@ -0,0 +1,38 @@
+const TelegramUser = require("../models/TelegramUser");
+const TelegramClient = require("../scripts/utils/TelegramClient");
+
+// TODO: Hiç bir fonksiyonun hata mesajı doğru bir şekilde gelmiyor. Program hata versede kullanıcıya basarılı cevabı dönüyor. Bunların düzeltilmesi lazım.
+// TODO: Api_key statik olarak giriliyor, frontendde cookiye konulabilir cookie üzerindne değer çekilebilir.
+
+/**
+ * Create a new telegram user via client credentials.
+ *
+ * @param {api_key , api_hash , phone_number} req
+ * @param {message, user} res
+ * @param {error} next
+ * @returns
+ */
+const getUserTelegramCredentials = async (req, res, next) => {
+  const { api_key, api_hash } = req.body;
+
+  try {
+    const user = await TelegramUser.create({
+      api_key: api_key,
+      api_hash: api_hash,
+    });
+
+    return res.status(201).json({
+      message: "Your telegram credentials were saved successfully",
+      user: {
+        api_key: user.api_key,
+        api_hash: user.api_hash,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  getUserTelegramCredentials,
+};
