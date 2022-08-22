f6299e7611590580cf1354dcf1d955fb2a127961
canbulgay <canbulgay@outlook.com>
2022-08-22 12:26:56 +0300

    feat(jta): Add sendCodeToPhoneNumber post method in TelegramClientRoute.

--- v1/src/routes/TelegramClient.js
+++ v1/src/routes/TelegramClient.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 const {
   getUserTelegramCredentials,
+  sendCodeToPhoneNumber,
 } = require("../controllers/TelegramClientController");
 
 router.post("/get-client-credentials", getUserTelegramCredentials);
+router.post("/send-code", sendCodeToPhoneNumber);
 module.exports = router;
