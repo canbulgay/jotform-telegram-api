ce64b37adb826ec79bb4cda10f34ff781530ae9a
canbulgay <canbulgay@outlook.com>
2022-08-22 12:30:10 +0300

    feat(jta): Add startTelegramClient  post method in TelegramClientRoute.

--- v1/src/routes/TelegramClient.js
+++ v1/src/routes/TelegramClient.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const {
   getUserTelegramCredentials,
+  startTelegramClientWithPhoneCode,
   sendCodeToPhoneNumber,
 } = require("../controllers/TelegramClientController");
 
 router.post("/get-client-credentials", getUserTelegramCredentials);
 router.post("/send-code", sendCodeToPhoneNumber);
+router.post("/start-telegram-client", startTelegramClientWithPhoneCode);
 module.exports = router;
