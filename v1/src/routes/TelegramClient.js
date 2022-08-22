2cd6adc3463cc2ad34e0e7bdaffea180fdb6a85f
canbulgay <canbulgay@outlook.com>
2022-08-22 12:32:34 +0300

    feat(jta): Add send-message  post method in TelegramClientRoute.

--- v1/src/routes/TelegramClient.js
+++ v1/src/routes/TelegramClient.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const {
   getUserTelegramCredentials,
   startTelegramClientWithPhoneCode,
+  sendMessage,
   sendCodeToPhoneNumber,
 } = require("../controllers/TelegramClientController");
 
 router.post("/get-client-credentials", getUserTelegramCredentials);
 router.post("/send-code", sendCodeToPhoneNumber);
 router.post("/start-telegram-client", startTelegramClientWithPhoneCode);
+router.post("/send-message", sendMessage);
 module.exports = router;
