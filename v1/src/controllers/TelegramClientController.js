c383a84ae4af7dd4d65b6e0cf2fab43db5537547
canbulgay <canbulgay@outlook.com>
2022-08-24 17:46:20 +0300

    refactor(jta): Change the logic in TelegramClientController

--- v1/src/controllers/TelegramClientController.js
+++ v1/src/controllers/TelegramClientController.js
@@ -1,32 +1,24 @@
-const TelegramUser = require("../models/TelegramUser");
 const TelegramClient = require("../scripts/utils/TelegramClient");
 
 // TODO: Hiç bir fonksiyonun hata mesajı doğru bir şekilde gelmiyor. Program hata versede kullanıcıya basarılı cevabı dönüyor. Bunların düzeltilmesi lazım.
-// TODO: Api_key statik olarak giriliyor, frontendde cookiye konulabilir cookie üzerindne değer çekilebilir.
 
 /**
  * Create a new telegram user via client credentials.
  *
- * @param {api_key , api_hash , phone_number} req
- * @param {message, user} res
+ * @param {api_key , api_hash} req
+ * @param {message} res
  * @param {error} next
  * @returns
  */
-const getUserTelegramCredentials = async (req, res, next) => {
+const setUserTelegramCredentials = async (req, res, next) => {
   const { api_key, api_hash } = req.body;
 
   try {
-    const user = await TelegramUser.create({
-      api_key: api_key,
-      api_hash: api_hash,
-    });
+    const client = new TelegramClient();
+    await client.saveCredentials(api_key, api_hash);
 
     return res.status(201).json({
-      message: "Your telegram credentials were saved successfully",
-      user: {
-        api_key: user.api_key,
-        api_hash: user.api_hash,
-      },
+      message: "Your credentials has been saved.",
     });
   } catch (error) {
     next(error);
@@ -46,36 +38,11 @@ const sendCodeToPhoneNumber = async (req, res, next) => {
   const { phone_number } = req.body;
 
   try {
-    const user = await TelegramUser.findOne({ api_key: 14961319 });
-    const client = new TelegramClient(user);
+    const client = new TelegramClient();
     await client.sendCode(phone_number);
 
     return res.status(200).json({
-      message: "Your code was sent successfully to your phone",
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
-/**
- * We sent a code to user via sendCodeToPhoneNumber request. This method will start the telegram client with the phone code.
- *
- * @param {phone_code} req
- * @param {message} res
- * @param {error} next
- * @return json
- */
-const startTelegramClientWithPhoneCode = async (req, res, next) => {
-  const { phone_code } = req.body;
-
-  try {
-    const user = await TelegramUser.findOne({ api_key: 14961319 });
-    const client = new TelegramClient(user);
-    await client.startClient(phone_code);
-
-    return res.status(200).json({
-      message: "Your telegram client was started successfully",
+      message: "Your code sent to your phone.",
     });
   } catch (error) {
     next(error);
@@ -94,8 +61,7 @@ const startTelegramClientWithPhoneCode = async (req, res, next) => {
 const sendMessage = async (req, res, next) => {
   const { username, message } = req.body;
   try {
-    const user = await TelegramUser.findOne({ api_key: 14961319 });
-    const client = new TelegramClient(user);
+    const client = new TelegramClient();
     await client.sendMessage(username, message);
 
     return res.status(200).json({
@@ -120,12 +86,54 @@ const sendMessage = async (req, res, next) => {
  */
 const terminateSessions = async (req, res, next) => {
   try {
-    const user = await TelegramUser.findOne({ api_key: 14961319 });
-    const client = new TelegramClient(user);
+    const client = new TelegramClient();
     await client.resetAuthorizations();
 
     return res.status(200).json({
-      message: "All sessions were terminated successfully",
+      message: "All sessions were terminated.",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ * Sign in the user via phone_code
+ *
+ * @param {phone_code} req
+ * @param {message} res
+ * @param {error} next
+ * @return json
+ */
+const signInTheUser = async (req, res, next) => {
+  const { phone_code } = req.body;
+  try {
+    const client = new TelegramClient();
+    await client.signIn(phone_code);
+
+    return res.status(200).json({
+      messsage: "You are logged in.",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+/**
+ * Logged out the authenticated user.
+ *
+ * @param {message} res
+ * @param {error} next
+ * @return json
+ */
+
+const logOutTheUser = async (req, res, next) => {
+  try {
+    const client = new TelegramClient();
+    await client.logOut();
+
+    return res.status(200).json({
+      message: "You are logged out.",
     });
   } catch (error) {
     next(error);
@@ -133,9 +141,10 @@ const terminateSessions = async (req, res, next) => {
 };
 
 module.exports = {
-  getUserTelegramCredentials,
+  setUserTelegramCredentials,
   sendCodeToPhoneNumber,
-  startTelegramClientWithPhoneCode,
   sendMessage,
   terminateSessions,
+  signInTheUser,
+  logOutTheUser,
 };
