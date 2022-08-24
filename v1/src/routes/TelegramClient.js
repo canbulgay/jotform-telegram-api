fb123103ed9f3654afc7ae4fef34803c120cde42
canbulgay <canbulgay@outlook.com>
2022-08-24 17:49:14 +0300

    refactor(jta): Modified TelegramClient routes

--- v1/src/routes/TelegramClient.js
+++ v1/src/routes/TelegramClient.js
@@ -2,17 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  getUserTelegramCredentials,
-  startTelegramClientWithPhoneCode,
+  setUserTelegramCredentials,
   sendMessage,
   sendCodeToPhoneNumber,
   terminateSessions,
+  signInTheUser,
+  logOutTheUser,
 } = require("../controllers/TelegramClientController");
 
-router.post("/get-client-credentials", getUserTelegramCredentials);
+router.post("/set-client-credentials", setUserTelegramCredentials);
 router.post("/send-code", sendCodeToPhoneNumber);
-router.post("/start-telegram-client", startTelegramClientWithPhoneCode);
+router.post("/sign-in", signInTheUser);
 router.post("/send-message", sendMessage);
 router.get("/terminate-sessions", terminateSessions);
+router.get("/logout", logOutTheUser);
 
 module.exports = router;
