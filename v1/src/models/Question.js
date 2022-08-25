745e41306a67ef2beb916ae6ad89459a963d9287
canbulgay <canbulgay@outlook.com>
2022-08-26 01:34:30 +0300

    feat(jta): Question model created.

---
+++ v1/src/models/Question.js
@@ -0,0 +1,33 @@
+const Mongoose = require("mongoose");
+
+const QuestionSchema = new Mongoose.Schema(
+  {
+    form_id: {
+      type: String,
+      required: true,
+    },
+    qid: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+    },
+    type: {
+      type: String,
+    },
+    required: {
+      type: String,
+    },
+    validation: {
+      type: String,
+      default: "None",
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+module.exports = Mongoose.model("Question", QuestionSchema);
