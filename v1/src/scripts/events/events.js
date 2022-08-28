a220e4ffac396cacf6b810460aa9e2eae5cad070
canbulgay <canbulgay@outlook.com>
2022-08-28 23:32:56 +0300

    feat(jta): Add form_id params to fetch:questions event in events.js

--- v1/src/scripts/events/events.js
+++ v1/src/scripts/events/events.js
@@ -2,16 +2,16 @@ const eventEmitter = require("./eventEmitter");
 const { getFormQuestions } = require("../utils/jotform/axios");
 const {
   saveQuestionsToDB,
-  checkIfQuestionsExist,
+  checkIfQuestionsNotExist,
 } = require("../utils/jotform/QuestionHelper");
 
 module.exports = () => {
-  eventEmitter.on("fetch:questions", async (formId) => {
+  eventEmitter.on("fetch:questions", async (formId, username) => {
     try {
-      if (await checkIfQuestionsExist(formId)) {
+      if (await checkIfQuestionsNotExist(formId, username)) {
         const response = await getFormQuestions(formId);
         const questions = response.data?.content;
-        await saveQuestionsToDB(questions, formId);
+        await saveQuestionsToDB(questions, formId, username);
       }
       return;
     } catch (error) {
