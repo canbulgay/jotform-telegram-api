f4dc8e0210d4cde230608d98542e76422c56ab3f
canbulgay <canbulgay@outlook.com>
2022-08-30 22:33:12 +0300

    fix(jta): Fix bug in post method

--- v1/src/scripts/utils/jotform/axios.js
+++ v1/src/scripts/utils/jotform/axios.js
@@ -2,17 +2,27 @@ const axios = require("axios");
 
 const jotformToken = "088669390670debab69ffc56927e0801";
 
-const getFormQuestions = (formId) => {
-  return axios.get(
-    `https://api.jotform.com/form/${formId}/questions?apiKey=${jotformToken}`
-  );
+const getFormQuestions = (formId) => async (_req, _res, next) => {
+  try {
+    return await axios.get(
+      `https://api.jotform.com/form/${formId}/questions?apiKey=${jotformToken}`
+    );
+  } catch (error) {
+    next(error);
+  }
 };
 
-const submitFormSubmissions = (formId, submissions) => {
-  return axios.post(
-    `https://api.jotform.com/form/${formId}/submission?apiKey=${jotformToken}`,
-    submissions
-  );
+const submitFormSubmissions = async (formId, submissions) => {
+  try {
+    const result = await axios.post(
+      `https://api.jotform.com/form/${formId}/submissions?apiKey=${jotformToken}`,
+      submissions
+    );
+    console.log(result.data);
+    return result.data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 module.exports = {
   getFormQuestions,
