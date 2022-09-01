208dd3637375ff5a440662c6f4480ee1ce6f503b
canbulgay <canbulgay@outlook.com>
2022-09-01 13:11:34 +0300

    feat(jta): Add phone_control type for prepare submissions before push

--- v1/src/scripts/utils/jotform/SubmissionHelper.js
+++ v1/src/scripts/utils/jotform/SubmissionHelper.js
@@ -4,7 +4,7 @@ const pushSubmissionsToJotform = async (submissions, formId) => {
   const submissionParams = prepareSubmissionsBeforePushing(submissions);
   const data = await submitFormSubmissions(formId, submissionParams);
 
-  const submissionId = data.content.submissionID;;
+  const submissionId = data.content.submissionID;
   return submissionId;
 };
 
@@ -23,6 +23,16 @@ const prepareSubmissionsBeforePushing = (submissions) => {
           fullName[1]
         );
       }
+    } else if (submission.type === "control_phone") {
+      if (!submission.answer.startsWith("9")) {
+        submission.answer = "9" + submission.answer;
+      }
+      const country = submission.answer.substring(0, 2);
+      const area = submission.answer.substring(2, 5);
+      const phone = submission.answer.substring(5);
+      submissionParams.append(`submission[${submission.qid}_country]`, country);
+      submissionParams.append(`submission[${submission.qid}_area]`, area);
+      submissionParams.append(`submission[${submission.qid}_phone]`, phone);
     } else {
       submissionParams.append(
         `submission[${submission.qid}]`,
